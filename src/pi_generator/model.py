40e7717866fc1ba0316dd9a6f0a2ddc0a173102a
Rico Li <rch0421@gmail.com>
2024-07-10 12:29:42 +0800

    feat: positional encoding added

--- src/pi_generator/model.py
+++ src/pi_generator/model.py
@@ -1,7 +1,59 @@
+import math
+
 import torch
 import torch.nn as nn
 
 from pi_generator.utils import reparameterize
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, embed_dim: int = 1, max_len: int = 5000):
+        super().__init__()
+        self.embed_dim = embed_dim
+        if embed_dim > 1:
+            pe = torch.zeros(max_len, embed_dim)  # (max_len, embed_dim)
+            position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(
+                1
+            )  # (max_len, 1)
+            div_term = torch.exp(
+                torch.arange(0, embed_dim, 2).float() * (-math.log(10000.0) / embed_dim)
+            ).unsqueeze(0)  # (1, embed_dim/2)
+            pe[:, 0::2] = torch.sin(position * div_term)  # (max_len, embeddim / 2)
+            pe[:, 1::2] = torch.cos(position * div_term)
+        elif embed_dim == 1:
+            pe = torch.zeros(max_len)  # (max_len)
+            position = torch.arange(0, max_len / 2, dtype=torch.float)  # (max_len/2,)
+            div_term = 1 / 10000.0
+            pe[0::2] = torch.sin(position * div_term)
+            pe[1::2] = torch.cos(position * div_term)
+        else:
+            raise ValueError
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)  # not learnable
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.embed_dim > 1:
+            return x + self.pe[:, : x.size(1), :]
+
+        return x + self.pe[:, : x.size(1)]
+
+
+class LearnablePositionalEncoding(nn.Module):
+    def __init__(self, embed_dim: int = 1, max_len: int = 5000):
+        super().__init__()
+        self.embed_dim = embed_dim
+        if embed_dim > 1:
+            self.pe = nn.Parameter(torch.zeros(1, max_len, embed_dim))
+        elif embed_dim == 1:
+            self.pe = nn.Parameter(torch.zeros(1, max_len))
+        else:
+            raise ValueError
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.embed_dim > 1:
+            return x + self.pe[:, : x.size(1), :]
+        return x + self.pe[:, : x.size(1)]
 
 
 class TransformerEncoder(nn.Module):
@@ -10,6 +62,7 @@ class TransformerEncoder(nn.Module):
         # linear trans from raw input to embedding
         # y = Mx + b, where input_dim depends on the src, and embed_dim decides by me
         self.embedding = nn.Linear(input_dim, embed_dim)
+        self.positional_encoding = LearnablePositionalEncoding()
         # notice that the output dim of above linear trans should be able to take
         # as input for transformer, that us d_model = embed_dim
         encoder_layer = nn.TransformerEncoderLayer(
@@ -30,6 +83,7 @@ class TransformerEncoder(nn.Module):
         self, src: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         src = self.embedding(src)
+        src = self.positional_encoding(src)
         output = self.transformer_encoder(src)
         mu = self.mu(output)
         logvar = self.logvar(output)
@@ -47,6 +101,7 @@ class TransformerDecoder(nn.Module):
     ):
         super().__init__()
         self.embedding = nn.Linear(input_dim, embed_dim)
+        self.positional_encoding = LearnablePositionalEncoding()
         decoder_layer = nn.TransformerDecoderLayer(
             d_model=embed_dim,
             nhead=nhead,
@@ -62,6 +117,7 @@ class TransformerDecoder(nn.Module):
     def forward(self, tgt: torch.Tensor, memory: torch.Tensor) -> torch.Tensor:
         # tgt is our desired output, memory is the output from encoder
         embed_tgt = self.embedding(tgt)
+        embed_tgt = self.positional_encoding(embed_tgt)
         decode_output = self.transformer_decoder(embed_tgt, memory)
         return self.fc_out(decode_output)
 
